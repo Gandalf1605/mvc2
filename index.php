473f14dc6fb765c02051f9644f52d1e339840b15
Vitaliy Malyshev <paruskramatorsk@gmail.com>
2018-08-31 14:43:02 +0300

    mvc

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: parus
- * Date: 29.08.2018
- * Time: 18:05
- */
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+define('ROOT', dirname(__FILE__));
+
+require_once (ROOT . '/components/Router.php');
+require_once (ROOT . '/components/Db.php');
+require_once (ROOT . '/components/View.php');
+
+
+$router = new Router();
+$router->run();
